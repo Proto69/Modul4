cd47bfcfcba6394f656bd5089f2bbcf62e18c1a0
Proto69 <timkaq67@gmail.com>
2022-06-27 15:05:10 +0300

    ...

---
+++ 03.3 3та задача.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            if (n > m)
+                throw new Exception("No solution");
+            var queue = new Queue<int>();
+            var stack = new Stack<int>();
+            stack.Push(m);
+            while (n != m)
+            {
+                if (m % 2 == 0 && m / 2 >= n)
+                {
+                    m /= 2;
+                    stack.Push(m);
+                }
+                else if (m - 2 >= n)
+                {
+                    m -= 2;
+                    stack.Push(m);
+                }
+                else if (m - 1 >= n)
+                {
+                    m -= 1;
+                    stack.Push(m);
+                }
+            }
+            Console.WriteLine(string.Join(" -> ", stack));
+        }
+    }
+}
