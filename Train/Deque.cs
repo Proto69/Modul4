2d704b0de586c4b6accfe671efa08ebcfe96e040
Proto69 <timkaq67@gmail.com>
2022-06-30 14:16:07 +0300

    ...

---
+++ Train/Deque.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainTest
+{
+    class Deque<T>
+    {
+        private Stack<T> frontBuffer = new Stack<T>();
+        private Stack<T> backBuffer = new Stack<T>();
+
+        public int BackCount { get; private set; }
+        public int FrontCount { get; private set; }
+        public int Count
+        {
+            get
+            {
+                return BackCount + FrontCount;
+            }
+        }
+
+        public void AddBack(T item)
+        {
+            backBuffer.Push(item);
+            BackCount++;
+        }
+
+        public void AddFront(T item)
+        {
+            frontBuffer.Push(item);
+            FrontCount++;
+        }
+
+        public T GetFront()
+        {
+            try
+            {
+                return frontBuffer.Peek();
+            }
+            catch { return default; }
+        }
+
+        public T GetBack()
+        {
+            try
+            {
+                return backBuffer.Peek();
+            }
+            catch { return default; }
+        }
+
+        public T RemoveBack()
+        {
+            return backBuffer.Pop();
+        }
+
+        public T RemoveFront()
+        {
+            return frontBuffer.Pop();
+        }
+    }
+}
---
+++ Train/Train.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainTest
+{
+    class Train
+    {
+        private int number;
+        private string name;
+        private string type;
+        private int cars;
+
+        public Train(int number, string name, string type, int cars)
+        {
+            this.number = number;
+            this.name = name;
+            Type = type;
+            Cars = cars;
+        }
+
+        public override string ToString()
+        {
+            return $"{number} {name} {Type} {Cars}";
+        }
+
+        public string Type { get; set; }
+
+        public int Cars { get; set; }
+    }
+}
